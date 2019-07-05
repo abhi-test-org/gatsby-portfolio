c738c4eb64d7090c2e9e4dd6c447933268f99ad0
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-07-05 12:58:13 -0700

    make a change

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,6 +20,7 @@ const About = ({ data: { about } }) => (
             __html: about.bioNode.childMarkdownRemark.html,
           }}
         />
+        YOOOOOO
       </div>
     </article>
   </Layout>
