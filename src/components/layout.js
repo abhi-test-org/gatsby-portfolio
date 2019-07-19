c66a3cb35ebfe2a8aca7c910f34831387cfb4e7b
Abhi Aiyer <abhiaiyer91@gmail.com>
2019-07-19 13:36:25 -0700

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -74,7 +74,7 @@ const TemplateWrapper = ({ children }) => (
               > </a>
             ))}
           </p>
-          <p>sudeee</p>
+          <p>sudee!e</p>
           <div className="sidebar__copyright">{data.datoCmsHome.copyright}</div>
         </div>
       </div>
